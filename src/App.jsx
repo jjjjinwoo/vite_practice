ff3d38e5eea6e77301ba25cce3d729b3f7ba2edd
jjjjinwoo <somes228@gmail.com>
2025-03-28 16:24:18 +0900

    feat:0328_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
-import Table from './pages/Table'
 import Map1 from './pages/Map1'
 import Map2 from './pages/Map2'
 import Map3 from './pages/Map3'
@@ -14,7 +13,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/table" element={<Table />} />
         <Route path="/map1" element={<Map1 />} />
         <Route path="/map2" element={<Map2 />} />
         <Route path="/map3" element={<Map3 />} />
@@ -24,7 +22,6 @@ function App() {
       </Routes>
 
       <Link to='/'>Go to Home</Link> <br/>
-      <Link to='/table'>Go to Table</Link> <br/>
       <Link to='/map1'>Go to Map1</Link> <br/>
       <Link to='/map2'>Go to Map2</Link> <br/>
       <Link to='/map3'>Go to Map3</Link> <br/>
